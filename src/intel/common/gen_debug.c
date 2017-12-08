8af9b03bac4e931f1ade0f9b4b719b8b88ecd74f
Kevin Rogovin <kevin.rogovin@intel.com>
2017-12-08 10:59:17 +0200

    intel/common:add debug flag for adding and checking padding on BO's

--- src/intel/common/gen_debug.c
+++ src/intel/common/gen_debug.c
@@ -85,6 +85,7 @@ static const struct debug_control debug_control[] = {
    { "nohiz",       DEBUG_NO_HIZ },
    { "color",       DEBUG_COLOR },
    { "reemit",      DEBUG_REEMIT },
+   { "check_oob",   DEBUG_CHECK_OOB },
    { NULL,    0 }
 };
 
